4bb7c0793b4ff3ed69b10b4b962ffbedd4779a10
GoodOldWes <weslaycock@ymail.com>
2024-09-18 12:15:50 -0700

    we are refractoring to make goldObject great again

--- src/Pages/CountryIndex.tsx
+++ src/Pages/CountryIndex.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import goldObjectWithFlags from '../utilities/goldObjectWithFlags';
 import CountryList from '../Pages/CountryList'; // Ensure this path is correct
 
+// import QuestionLogicForm from '../QuestionLogicForm';
+
 const CountryIndex: React.FC = () => {
     return (
         <div>
             <h1>Country Index</h1>
-            <CountryList countries={goldObjectWithFlags} />
+            {/* <CountryList countries={goldObjectWithFlags} /> */}
+            <CountryList countries={filteredCountries} />
         </div>
     );
 };
