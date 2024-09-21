48815eac2c43d5cb0dbe74fdc36e620645f66e0a
GoodOldWes <weslaycock@ymail.com>
2024-09-20 19:41:54 -0700

    0.1.0 PL merging master to branch so I can work not on the master directly

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import './About.css';
 
 const About: React.FC = () => {
     return(
-        <div>
+        <div className='aboutBody'>
             <h1>About Page</h1>
-            <p>Learn More about us here!</p>
+            <p className='aboutText' >
+                UUUGHHHH!!!! Picking a country is a challenging task, we have to consider things like budget, language barrier, safety, visa policy, and much more. If only we had a list of countries that matched all our needs. Well, nice to meet you, here at First Step Country, we set out to do just that! 
+
+Do a short and fun questionnaire, and discover every country that matches your needs! It’s fun!!!
+            </p>
+            
         </div>
     );
 };
