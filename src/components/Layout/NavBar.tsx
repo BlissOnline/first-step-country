a4714d2402a6df7aaf6f7019f5e2679a3ff5f068
GoodOldWes <weslaycock@ymail.com>
2025-05-08 14:35:20 -0700

    just about to test on serve npm before putting on hoststinger

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -14,7 +14,7 @@ const NavBar: React.FC = () => {
                 {/* <li><Link to="/">Home</Link></li> */}
                 {/* <li><Link to="/blog">Blog</Link></li> */}
                 <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                {/* <li><Link to="/contact">Contact</Link></li> */}
             </ul>
         </nav>
     );
