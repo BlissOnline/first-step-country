8a90741553d5d1db4dfcdcd7d222f55bd0922053
GoodOldWes <weslaycock@ymail.com>
2025-05-11 00:20:57 -0700

    solved public photos for shares bug

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -18,7 +18,7 @@ const Home: React.FC = () => {
                 <meta property="og:title" content="Find Your Ideal Travel Destination – Take the Quiz!" />
                 <meta property="og:description" content="Answer a few questions and uncover the perfect country for your next adventure. Start exploring now!" />
                 {/* <meta property="og:image" content="../assets/images/ppm-graphic.png" /> */}
-                <meta property="og:image" content="/ppm-graphic.png" />
+                <meta property="og:image" content="https://firststepcountry.com/ppm-graphic.png" />
                 <meta property="og:url" content="https://firststepcountry.com/"  />
                 <meta property="og:type" content="quiz"  />
                 <link rel="canonical" href="https://firststepcountry.com/" />
@@ -28,7 +28,7 @@ const Home: React.FC = () => {
                 <meta name="twitter:title" content="Find Your Ideal Travel Destination – Take the Quiz!" />
                 <meta name="twitter:description" content="Answer a few questions and uncover the perfect country for your next adventure. Start exploring now!" />
                 {/* <meta name="twitter:image" content="../assets/images/ppm-graphic.png" /> */}
-                <meta name="twitter:image" content="/ppm-graphic.png" />
+                <meta name="twitter:image" content="https://firststepcountry.com/ppm-graphic.png" />
                 <meta name="twitter:url" content="https://firststepcountry.com/" />
 
             </Helmet>
