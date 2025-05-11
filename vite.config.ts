61c92086f0256976875d8af5870d21523a2a54da
GoodOldWes <weslaycock@ymail.com>
2025-05-11 16:00:26 -0700

    need a sitemap and register on google but need to make seperate url for questions

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://firststepcountry.com/', 
+      dynamicRoutes: ['/questions', '/about'], // Add more dynamic routes if needed
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src', // Adjust based on your project structure
