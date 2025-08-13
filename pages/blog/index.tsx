a5e09afc4a67c1b177baae31cc9c6e2eb55a92e0
GoodOldWes <weslaycock@ymail.com>
2025-08-13 13:56:52 -0700

    working blog selector page nice

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,28 +1,53 @@
 // pages/blog/index.tsx
 import Link from 'next/link'
 import type { GetStaticProps } from 'next'
-import { listSlugs } from '../../lib/posts'
+import { listSlugs, getPostBySlug } from '../../lib/posts'
+import styles from '@/components/blog-components/BlogSelector.module.css'
+
+type PostCard = {
+  slug: string
+  displayTitle: string
+  featuredImage: string
+}
 
 type Props = {
-  slugs: string[]
+  posts: PostCard[]
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const slugs = listSlugs()
-  return { props: { slugs } }
+  const posts = slugs.map((slug) => {
+    const { frontMatter } = getPostBySlug(slug)
+    return {
+      slug,
+      displayTitle: frontMatter.displayTitle ?? frontMatter.title,
+      featuredImage: frontMatter.featuredImage || '/default.jpg',
+    }
+  })
+  return { props: { posts } }
 }
 
-export default function BlogIndex({ slugs }: Props) {
+export default function BlogIndex({ posts }: Props) {
   return (
-    <main className="prose mx-auto p-8">
-      <h1>Blog</h1>
-      <ul>
-        {slugs.map((slug: string) => (
-          <li key={slug}>
-            <Link href={`/blog/${slug}`}>{slug}</Link>
-          </li>
+    <main className="mx-auto p-8">
+      <h1>Budget Travel Blogs</h1>
+
+      <div className={styles.blogGrid}>
+        {posts.map(({ slug, displayTitle, featuredImage }) => (
+          <Link
+            key={slug}
+            href={`/blog/${slug}`}
+            className={styles.card}
+          >
+            <div
+              className={styles.cardImage}
+              style={{ backgroundImage: `url(${featuredImage})` }}
+            />
+            <div className={styles.cardTitle}>{displayTitle}</div>
+            <div className={styles.frame} />
+          </Link>
         ))}
-      </ul>
+      </div>
     </main>
   )
 }
